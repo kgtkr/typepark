7441edf506983cd73621b43b50c33dcd010f45d3
kgtkr <kgtkr.jp@gmail.com>
2018-09-07 20:53:43 +0900

    ts@nextとかgroup

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,16 @@ export type Concat<A extends any[], B extends any[], R extends any[]=[]> = {
   1: Concat<Tail<A>, B, Cons<Head<A>, R>>
   2: Concat<A, Tail<B>, Cons<Head<B>, R>>
 }[A extends [] ? B extends [] ? 0 : 2 : 1];
-
 export type Zip<A extends any[], B extends any[], R extends any[]=[]> = {
   0: Reverse<R>,
   1: Zip<Tail<A>, Tail<B>, Cons<[Head<A>, Head<B>], R>>
 }[A extends [] ? 0 : B extends [] ? 0 : 1];
-
 export type Take<N extends number, T extends any[], R extends any[]=[]> = {
   0: Reverse<R>,
   1: Take<N, Tail<T>, Cons<Head<T>, R>>
 }[T extends [] ? 0 : R["length"] extends N ? 0 : 1];
+export type Group<N extends number, T extends any[], R1 extends any[]=[], R2 extends any[]=[]> = {
+  0: Reverse<R2>,
+  1: Group<N, T, [], Cons<Reverse<R1>, R2>>,
+  2: Group<N, Tail<T>, Cons<Head<T>, R1>, R2>
+}[T extends [] ? R1 extends [] ? 0 : 1 : (R1["length"] extends N ? 1 : 2)];
