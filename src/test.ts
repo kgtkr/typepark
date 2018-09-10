79919058c82ee12395d90f47073c1bcd8a68209f
kgtkr <kgtkr.jp@gmail.com>
2018-09-10 15:56:37 +0900

    assertType

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,4 @@
 type ComparableType<T> = [T];
 export type TypeEq<A, B> = ComparableType<A> extends ComparableType<B> ? ComparableType<B> extends ComparableType<A> ? true : false : false;
+
+export function assertType<_T extends true>() { }
