ce3f0cac737bb98419fa9e258159c52bff197eb2
kgtkr <kgtkr.jp@gmail.com>
2018-09-10 17:27:58 +0900

    バグ修正

--- src/test.ts
+++ src/test.ts
@@ -6,6 +6,6 @@ type ComparableType<T> = [T];
 type TypeEqNotAny<A, B> = TypeEqNotUnion<ComparableType<A>, ComparableType<B>>;
 type IsAny<T> = And<TypeEqNotAny<T, 1>, TypeEqNotAny<T, 2>>;
 type IsNotAny<T> = Not<IsAny<T>>;
-export type TypeEq<A, B> = Or<And<IsAny<A>, IsAny<B>>, And<And<IsNotAny<A>, IsNotAny<B>>, TypeEqNotAny<A, B>>>
+export type TypeEq<A, B> = Or<And<IsAny<A>, IsAny<B>>, And<And<IsNotAny<A>, IsNotAny<B>>, TypeEqNotAny<A, B>>>;
 
 export function assertType<_T extends true>() { }
