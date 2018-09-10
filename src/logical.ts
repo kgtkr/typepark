a4896a7c72fe54ff89ed6c0871366ae70545bd52
kgtkr <kgtkr.jp@gmail.com>
2018-09-10 17:26:13 +0900

    論理演算

--- src/logical.ts
+++ src/logical.ts
@@ -1,4 +1,4 @@
-export type And<A extends boolean, B extends boolean> = A extends true ? (B extends true ? true : false) : (B extends true ? false : false);
-export type Or<A extends boolean, B extends boolean> = A extends true ? (B extends true ? true : true) : (B extends true ? true : false);
+export type And<A extends boolean, B extends boolean> = A extends false ? (B extends true ? true : false) : false;
+export type Or<A extends boolean, B extends boolean> = A extends true ? true : (B extends true ? true : false);
 export type Xor<A extends boolean, B extends boolean> = A extends true ? (B extends true ? false : true) : (B extends true ? true : false);
 export type Not<X extends boolean> = X extends true ? false : true;
