057766b7c436c2ac5be7d4d09b8eb4b5f38e35e5
kgtkr <kgtkr.jp@gmail.com>
2018-09-22 09:37:56 +0900

    mapped type使うように修正

--- src/pipe.ts
+++ src/pipe.ts
@@ -5,10 +5,7 @@ type ShiftZip<T extends any[]> = Zip<T, Tail<T>>;
 
 type Tuple2Fn<T> = T extends [infer A, infer B] ? (x: A) => B : never;
 
-type _Pipe<T extends any[], R extends any[]=[]> = {
-  0: Reverse<R>,
-  1: _Pipe<Tail<T>, Cons<Tuple2Fn<Head<T>>, R>>
-}[T extends [] ? 0 : 1];
+type _Pipe<T extends any[]> = { [P in keyof T]: Tuple2Fn<T[P]> }
 
 export type PipeParams<T extends [any, any, ...any[]]> = Cast<_Pipe<Cast<ShiftZip<T>, any[], []>>, any[], []>;
 export type PipeResult<T extends [any, any, ...any[]]> = (x: Head<T>) => Last<T>;
