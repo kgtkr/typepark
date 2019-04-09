024d29091aa3480f45ecb93c52b8896fa72e13c0
kgtkr <kgtkr.jp@gmail.com>
2019-04-09 22:37:28 +0900

    fix #4 v0.3.2

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,12 +1,22 @@
-import { Zip, Tail, Head, Reverse, Cons, Last } from "./list";
+import { Zip, Tail, Head, Last } from "./list";
 import { Cast } from "./util";
+import { assertType, TypeEq } from "./test";
 
 type ShiftZip<T extends any[]> = Zip<T, Tail<T>>;
 
+assertType<TypeEq<ShiftZip<[1, 2, 3]>, [[1, 2], [2, 3]]>>();
+
+
 type Tuple2Fn<T> = T extends [infer A, infer B] ? (x: A) => B : never;
 
-type _Pipe<T extends any[]> = { [P in keyof T]: Tuple2Fn<T[P]> }
+assertType<TypeEq<Tuple2Fn<[1, 2]>, (x: 1) => 2>>();
 
-export type PipeParams<T extends [any, any, ...any[]]> = Cast<_Pipe<Cast<ShiftZip<T>, any[], []>>, any[], []>;
+type _Pipe<T extends any[]> = { [P in keyof T]: Tuple2Fn<T[P]> };
+
+assertType<TypeEq<_Pipe<[[1, 2], [2, 3]]>, [(x: 1) => 2, (x: 2) => 3]>>();
+
+export type PipeParams<T extends [any, any, ...any[]]> = Cast<_Pipe<ShiftZip<T> extends infer X ? Cast<X, any[], []> : never>, any[], []>;
 export type PipeResult<T extends [any, any, ...any[]]> = (x: Head<T>) => Last<T>;
 export type Pipe<T extends [any, any, ...any[]]> = (...f: PipeParams<T>) => PipeResult<T>;
+
+assertType<TypeEq<Pipe<[1, 2, 3, 4]>, (a: (x: 1) => 2, b: (x: 2) => 3, c: (x: 3) => 4) => ((x: 1) => 4)>>();
