7bb31e81f8af86e89fd4040c45b6e5edc66d61c5
kgtkr <kgtkr.jp@gmail.com>
2019-04-13 01:27:31 +0900

    get-in進捗

--- src/get-in.ts
+++ src/get-in.ts
@@ -1,14 +1,13 @@
 /*import { Concat, Tail, Head } from "./list";
 import { Cast } from "./util";
 
-type X = CursorEnum<Foo>;
-
 type GetIn<T> = <K extends CursorEnum<T>>(path: K) => CursorResult<T, Cast<K, any[]>>;
 
 type Y = CursorResult<Foo, ["foo", "x"]>;
 
-type CursorEnum<T, R extends any[]=[], Key=keyof T> = {
-  0: Key extends infer P ? R | CursorEnum<T[Cast<P, keyof T>], Cast<Concat<R, [P]>, any[]>> : never
+type CursorEnum<T, R extends any[] = [], Key = keyof T> = {
+  //R | (Key extends infer P ? CursorEnum<T[Cast<P, keyof T>], Concat<R, [P]> extends infer X ? Cast<X, any[]> : X> : never)
+  0: (CursorEnum<T[Cast<Key, keyof T>], Concat<R, [Key]> extends infer X ? Cast<X, any[]> : X>),
   1: R
 }[T extends object ? 0 : 1];
 
