fe91ddf55bf5f0bc0495935df24a07e547397ba8
kgtkr <kgtkr.jp@gmail.com>
2019-04-16 21:48:44 +0900

    テストケース修正

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -5,8 +5,6 @@ assertType<TypeEq<TypeEq<1, 1>, true>>();
 assertType<TypeEq<TypeEq<{}, { x: 1 }>, false>>();
 assertType<TypeEq<TypeEq<{ x: 1 }, {}>, false>>();
 
-assertType<TypeEq<TypeEq<{ x: 1, y: 1 }, { x: 1 } & { y: 1 }>, true>>();
-
 assertType<TypeEq<TypeEq<1, 2>, false>>();
 assertType<TypeEq<TypeEq<2, 1>, false>>();
 
