75e0a7342a526ade4866f1f791c27e2c00f54072
kgtkr <kgtkr.jp@gmail.com>
2019-04-16 21:55:27 +0900

    spec修正

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,4 @@
-import { And, Not, Or } from "./logical";
-
-type IsExtends<A, B> = A extends B ? true : false;
-type TypeEqNotUnion<A, B> = And<IsExtends<A, B>, IsExtends<B, A>>;
-type ComparableType<T> = [T];
-type TypeEqNotAny<A, B> = TypeEqNotUnion<ComparableType<A>, ComparableType<B>>;
-type IsAny<T> = And<TypeEqNotAny<T, 1>, TypeEqNotAny<T, 2>>;
-type IsNotAny<T> = Not<IsAny<T>>;
-export type TypeEq<A, B> = Or<And<IsAny<A>, IsAny<B>>, And<And<IsNotAny<A>, IsNotAny<B>>, TypeEqNotAny<A, B>>>;
+export type TypeEq<A, B> = (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true : false;
 
 export function assertType<_T extends true>() { }
 export function assertNotType<_T extends false>() { }
